666456b194bedcdcd51e80cb8c2b3916ae496ccd
ephemeral sadness <49094121+ephemeralsadness@users.noreply.github.com>
2020-06-07 18:36:23 +1000

    Реализация шаблонного класса Vector
    Реализован класс Vector

--- helper/Vector.h
+++ helper/Vector.h
@@ -4,25 +4,115 @@
 
 template <typename T>
 class Vector {
-    Vector() noexcept;
-    ~Vector() noexcept;
-    Vector(const Vector<T>& v) noexcept;
-    Vector<T>& operator = (const Vector<T>& v) noexcept;
-    Vector(Vector<T>&& v) noexcept;
-    Vector<T>& operator = (Vector<T>&& v) noexcept;
+public:
+    Vector() noexcept
+            : _size(0),
+              _capacity(0),
+              _data(nullptr) {}
 
-    T& operator [] (size_t index) noexcept;
-    const T& operator [] (size_t index) const noexcept;
+    ~Vector() noexcept {
+        delete[] _data;
+    }
 
-    void PushBack(T t) noexcept;
-    void PopBack() noexcept;
+    Vector(const Vector<T>& v) noexcept
+            : _size(v._size),
+              _capacity(v._size),
+              _data(nullptr) {
+        if (v._size == 0) return;
 
-    void Reserve() noexcept;
+        _data = new T[_capacity];
+        for (int i = 0; i < _size; ++i)
+            _data[i] = v._data[i];
+    }
 
-    T* Begin() noexcept;
-    const T* Begin() const noexcept;
-    T* End() noexcept;
-    const T* End() const noexcept;
+    Vector<T>& operator = (const Vector<T>& v) noexcept {
+        if (this == &v) return *this;
+        *this = std::move(Vector(v));
+        return *this;
+    }
 
-    size_t Size() const noexcept;
+    Vector(Vector<T>&& v) noexcept
+            : _size(v._size),
+              _capacity(v._capacity),
+              _data(v._data) {
+        v._size = 0;
+        v._capacity = 0;
+        v._data = nullptr;
+    }
+
+    Vector<T>& operator = (Vector<T>&& v) noexcept {
+        ~Vector();
+        _size = v._size;
+        _capacity = v._capacity;
+        _data = v._data;
+        v._size = 0;
+        v._capacity = 0;
+        v._data = nullptr;
+    }
+
+    T& operator [] (size_t index) noexcept {
+        return _data[index];
+    }
+
+    const T& operator [] (size_t index) const noexcept {
+        return _data[index];
+    }
+
+    void PushBack(T t) noexcept {
+        if (_size < _capacity) {
+            _data[_size++] = std::move(t);
+        } else {
+            _capacity = (_capacity == 0 ? 1 : _capacity * 2);
+            T* prev_data = _data;
+            _data = new T[_capacity];
+
+            _data[_size] = std::move(t);
+
+            for (int i = 0; i < _size; ++i)
+                _data[i] = std::move(prev_data[i]);
+
+            _size++;
+            delete[] prev_data;
+        }
+    }
+
+    void PopBack() noexcept {
+        _data[--_size].~T();
+    }
+
+    void Reserve(int capacity) noexcept {
+        if (capacity <= _capacity)
+            return;
+
+        _capacity = capacity;
+        T* prev_data = _data;
+        _data = new T[_capacity];
+
+        for (int i = 0; i < _size; ++i)
+            _data[i] = prev_data[i];
+
+        delete[] prev_data;
+    }
+
+    T* Begin() noexcept {
+        return _data;
+    }
+    const T* Begin() const noexcept {
+        return _data;
+    }
+    T* End() noexcept {
+        return _data + _size;
+    }
+    const T* End() const noexcept {
+        return _data + _size;
+    }
+
+    size_t Size() const noexcept {
+        return _size;
+    }
+
+private:
+    size_t _size;
+    size_t _capacity;
+    T* _data;
 };
