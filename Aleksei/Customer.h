7ebd778674014c327eee5e69a8b3775d62dc4d30
BLGALEX <58458024+BLGALEX@users.noreply.github.com>
2020-06-07 17:39:01 +1000

    Классы "Заказчик" и "Компания"
    Добавлены новые классы "Заказчик" и "Компания" с конструкторами по умолчанию, полными конструкторами, геттерами и сеттерами для полей класса.

---
+++ Aleksei/Customer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include<string>
+#include "Vector.h"
+
+class Customer {
+public:
+
+	Customer() :name("Empty"),
+				service("Empty"),
+				company_name("Empty"),
+				volume(0) {}
+
+	Customer(std::string n, std::string s, std::string c, uint16_t v) {
+		name = n;
+		service = s;
+		company_name = c;
+		volume = v;
+	}
+				
+	std::string GetName() const;
+	std::string GetService() const;
+	std::string GetCompanyName() const;
+	uint16_t GetVolume() const;
+
+	void SetName(std::string s);
+	void SetService(std::string s);
+	void SetCompanyName(std::string s);
+	void SetVolume(uint16_t v);
+
+private:
+	std::string name;
+	std::string service;
+	std::string company_name;
+	uint16_t volume;
+};
