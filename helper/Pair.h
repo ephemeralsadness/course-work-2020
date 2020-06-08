7ad752bbfa176d8d6a895ab5921a482b7f6d8c71
BLGALEX <58458024+BLGALEX@users.noreply.github.com>
2020-06-08 22:05:04 +1000

    Добавлен конструктор по умолчанию

--- helper/Pair.h
+++ helper/Pair.h
@@ -2,10 +2,9 @@
 
 template <typename T, typename U>
 struct Pair {
-    T first;
-    U second;
-
-    Pair(T first, U second)
-            : first(first),
-              second(second) {}
-};
+	T first;
+	U second;
+	Pair(T first, U second)
+		: first(first), second(second) {}
+	Pair(){}
+}; 
