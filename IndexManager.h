409871a9cda2af94228d389ca160335083aabbca
ephemeral sadness <49094121+ephemeralsadness@users.noreply.github.com>
2020-07-01 23:49:46 +1000

    Обновление интерфейса IndexManager

--- IndexManager.h
+++ IndexManager.h
@@ -1,64 +1,79 @@
 #pragma once
 
-#include "aleksei/RBTree.h"
 #include "alexander/RBTree.h"
-#include "aleksei/HashTable.h"
 #include "alexander/HashTable.h"
-
-namespace Reports {
-    struct CompanyIncomes {
-        std::string company;
-        double income;
-    };
-}
+#include "aleksei/RBTree.h"
+#include "aleksei/HashTable.h"
 
 
-
-
+/*
+ * Добавление
+ * - компании
+ * - заказа
+ * - услуги
+ * Удаление
+ * - компании
+ * - заказа
+ * - услуги
+ * Поиск
+ * - компании
+ * - заказа
+ * - цены услуги
+ * - длительность услуги
+ * Просмотр
+ * - компаний
+ * - заказов
+ * - цен услуг
+ * - длительность услуг
+ * Работа с файлами
+ * - сохранение
+ * - считывание
+ * Формирование отчетов
+ * - первый
+ * - второй
+ * - третий
+ * - четвертый
+ */
 class IndexManager {
 public:
-    Vector<Reports::CompanyIncomes> GetCompanyIncomes() const {
-        using Reports::CompanyIncomes;
-        Vector<CompanyIncomes> result;
-        result.Reserve(_companies.Size());
+	IndexManager() = default;
 
-        auto companies = _companies.LookUp();
-        for (auto it = companies.Begin(); it != companies.End(); ++it) {
-            result.PushBack({ it->first->GetName(), 0 });
-        }
+	void AddCompany(Company c);
+	void AddCustomer(Customer c);
+	void AddService(ServicePrice sp, ServiceDuration sd);
 
-        result.MergeSort([](const CompanyIncomes& lhs, const CompanyIncomes& rhs) {
-            return lhs.company < rhs.company;
-        });
+	void RemoveCompany(const std::string& name);
+	void RemoveCustomer(const std::string& name);
+	void RemoveService(const std::string& name);
 
-        auto customers = _customers.LookUp();
-        for (auto it = customers.Begin(); it != customers.End(); ++it) {
-            size_t company_index = companies.BinarySearch({it->first->GetName()});
-        }
+	const Company& FindCompany(const std::string& name);
+	const Company& FindCustomer(const std::string& name);
+	const Company& FindServicePrice(const std::string& name);
+	const Company& FindServiceDuration(const std::string& name);
 
+	Vector<Pair<const Company*, size_t>> LookUpCompanies();
+	Vector<const Customer*> LookUpCustomers();
+	Vector<const ServicePrice*> LookUpServicePrices();
+	Vector<Pair<const ServiceDuration*, size_t>> LookUpServiceDurations();
 
-        result.MergeSort([](const CompanyIncomes& lhs, const CompanyIncomes& rhs) {
-            return lhs.income > rhs.income;
-        });
-        return result;
-    }
+	void SaveData(const std::string& file_name);
+	void LoadData(const std::string& file_name);
 
-    Vector<Pair<Company, Vector<std::string>>> GetCompanyCustomers(
-            const std::string& company_name
-            ) const;
+	// vector of {company name, income of this company}
+	Vector<Pair<std::string, double>> GetCompaniesIncomes();
 
-    Vector<Pair<std::string, Pair<double, double>>> GetCustomerServiceDuration() const;
+	// vector of {company name, vector of its clients}
+	Vector<Pair<std::string, Vector<std::string>>> GetCompaniesClients();
 
-    Vector<std::string> GetCompany(const std::string& service) const;
+	// vector of {customer name, {min duration of its services, max duration of its services}}
+	Vector<Pair<std::string, Pair<double, double>>> GetCustomersServiceDurations();
 
-    bool Add(Company company);
-    bool Add(Customer customer);
-    bool Add(ServiceDuration service_duration);
-    bool Add(ServicePrice service_price);
+	// vector of {service, {companies, who perform this service}}
+	Vector<Pair<std::string, Vector<std::string>>> GetServiceCompanies();
 
 private:
-    Aleksei::RBTree _customers;
-    Aleksei::HashTable _companies;
-    Alexander::RBTree _service_prices;
-    Alexander::HashTable _service_durations;
+	Aleksei::HashTable _companies;
+	Aleksei::RBTree _customers;
+	Alexander::RBTree _service_prices;
+	Alexander::HashTable _service_durations;
 };
